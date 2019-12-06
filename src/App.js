8c22f1dcf1afea3016f7a4a0114987416c92d2c8
zxx <782265815@qq.com>
2019-12-06 17:46:27 +0800

    第一次提交, login页面

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ /**
+  * BrowserRouter : 没有 "#",
+  * HashRouter: 带有 "#",
+ */
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
+// 应用的根组件
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            
+            <BrowserRouter> {/**由于是第一层路由, 所以外层需要先写个路由器 */}
+                <Switch>    {/**只匹配其中一个 */}
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/" component={Admin}></Route>
+                </Switch>
+
+            </BrowserRouter>
+        )
+    }
 }
-
-export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import './login.less';
+import logo from './images/logo.jpg';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+//可以定义组件, 必须写在import后面
+//const Item = Form.Item;
+
+// 登录路由组件
+class Login extends Component {
+    handleSubmit = (e) => {
+        //阻止默认行为
+        e.preventDefault();
+        const form = this.props.form;
+        //获取表单项的输入数据
+        const values = form.getFieldsValue();
+        console.log(values);
+    }
+
+    
+    /**
+     *  @memberof Login
+     *  自定义密码验证规则
+     * 
+     */
+    validatorPwd = (rule, value, callback) =>{
+        console.log('validatorPwd()', rule, value);
+        if (!value) {
+            callback('必须输入密码!');
+        }else if (value.length < 5) {
+            callback('密码不能小于4位!');
+        }else if (value.length > 13) {
+            callback('密码不能大于12位!');
+        }else if (/^[a-zA-Z0-9_]+$/g.test(value)) {
+            callback('密码必须是英文、数字或下划线!');
+        }else{
+            callback()  //验证通过
+        }
+        
+        //callback('xxxxx')   //验证失败, 并指定提示的文本
+    }
+
+    render() {
+        //得到form对象
+        const form = this.props.form;
+        const { getFieldDecorator } = form;
+        return (
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="" />
+                    <h1>React：后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {
+                                getFieldDecorator('username', {
+                                    //声明式验证, 直接使用别人定义好的验证规则进行验证
+                                    rules: [
+                                        { require: true, whitespace: true, message: '必须输入用户名!' },
+                                        { min: 4, message: '用户名最少4位!' },
+                                        { max: 12, message: '用户名最多12位!' },
+                                        { pattern: /^[a-zA-Z0-9_]+$/g, message: '用户名必须是英文、数字或下划线!' },
+                                    ]
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        placeholder="Username"
+                                    />
+                                )
+                            }
+                        </Form.Item>
+                        <Form.Item>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        { 
+                                            validator:this.validatorPwd
+                                        }
+                                    ],
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="Password"
+                                    />,
+                                )
+                            }
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                        </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
+}
+
+/**
+ * 1.高阶函数   
+        1).一类特别的函数
+            a.接受函数类型的参数
+            b.返回值是函数
+        2).常见
+            a. 定时器: setTimeout()/setInterval()
+            b. Promise: Promise(() => {})   then(value => {}, reason => {})
+            c. 数组遍历相关的方法: forEach() / filter() / map() / find() findIndex()
+            d. fn.bind()
+            e. Form.create()(Login)
+        3).高阶函数更新状态, 更加具有扩展性
+ * 2.高阶组件
+        1).本质就是一个函数 
+        2).接受一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+        3).作用: 扩展组件的功能
+        4).作用: 高阶组件也是高阶函数, 接受一个组件函数, 返回的是一个新的组件函数
+ */
+
+//包装Form组件 生成一个新的组件 Form(Login)
+//新组件会向Form组件传递一个对象属性: form
+const wrapLogin = Form.create()(Login);
+export default wrapLogin;
